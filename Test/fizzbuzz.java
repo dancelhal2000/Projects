a07c5cee930f0bd5cdb4f52d5fa8586892dc61a0
dancel <aobinaobin15@gmail.com>
2026-02-10 18:11:22 +0700

    optime

--- Test/fizzbuzz.java
+++ Test/fizzbuzz.java
@@ -2,18 +2,16 @@ package Test;
 
 public class fizzbuzz {
     public static void main(String[] args) {
-        int x;
         for (int i = 0; i <= 50; i++) {
-            x = i;
-            if (x % 3 == 0 && x % 5 == 0) {
+            if (i % 3 == 0 && i % 5 == 0) {
                 System.out.println("fizzbuzz");
                 continue;
-            } else if (x % 3 == 0) {
+            } else if (i % 3 == 0) {
                 System.out.println("fizz");
-            } else if (x % 5 == 0) {
+            } else if (i % 5 == 0) {
                 System.out.println("buzz");
             } else {
-                System.out.println(x);
+                System.out.println(i);
             }
         }
     }
