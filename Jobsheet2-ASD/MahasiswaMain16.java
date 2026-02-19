f0328ced96b0d84f265409e0612af19cef35c6b9
dancelhal2000 <aobinaobin15@gmail.com>
2026-02-19 22:02:30 +0700

    add

---
+++ Jobsheet2-ASD/MahasiswaMain16.java
@@ -0,0 +1,15 @@
+public class MahasiswaMain16 {
+    public static void main(String[] args) {
+        Mahasiswa16 mhs1 = new Mahasiswa16();
+        mhs1.nama = "Muhamamd Ali Farhan";
+        mhs1.nim = "2241720171";
+        mhs1.kelas = "SI 2J";
+        mhs1.ipk = 3.55;
+
+        mhs1.tampilkanInformasi();
+        mhs1.ubahKelas("SI 2K");
+        mhs1.updateIPK(3.60);
+        mhs1.tampilkanInformasi();
+
+    }
+}
