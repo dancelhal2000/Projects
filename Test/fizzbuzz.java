fea35461748ee22373f8b2b1fbe3fc6d7cbc3ee3
dancelhal2000 <aobinaobin15@gmail.com>
2026-02-19 22:03:25 +0700

    add

---
+++ Test/fizzbuzz.java
@@ -0,0 +1,18 @@
+package Test;
+
+public class fizzbuzz {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 50; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("fizzbuzz");
+                continue;
+            } else if (i % 3 == 0) {
+                System.out.println("fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
