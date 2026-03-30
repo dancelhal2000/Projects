8afca4e8aa904aa49f6e19dc8ca88e7f27bbb8ea
dancel <aobinaobin15@gmail.com>
2026-03-30 20:06:32 +0700

    Add files via upload

---
+++ latihan/TwoSum.java
@@ -0,0 +1,23 @@
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] nums = { 2, 7, 11, 15 };
+        int target = 9;
+        twoSum(nums, target);
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        int numI = 0;
+        int numJ = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            numI = nums[i];
+            for (int j = i + 1; j < nums.length; j++) {
+                numJ = nums[j];
+                if (numI + numJ == target) {
+                    return new int[] { nums[i], nums[j] };
+                }
+            }
+        }
+        return new int[] {};
+    }
+}
