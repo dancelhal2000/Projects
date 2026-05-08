3eb8a3ea1e889bb220ea5e45cb6d68b4d3db41b1
dancel <aobinaobin15@gmail.com>
2026-05-08 10:25:49 +0700

    delete

--- ASD/Jobsheet/Jobsheet11-ASD/Pertemuan12/Node16.java
+++
@@ -1,9 +0,0 @@
-public class Node16 {
-    Mahasiswa16 data;
-    Node16 next;
-
-    public Node16(Mahasiswa16 data, Node16 next) {
-        this.data = data;
-        this.next = next;
-    }
-}   
